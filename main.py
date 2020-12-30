d85a6fac10355c324c3b62012bab02ea9c4ce867
Jacob <jacobneal65@gmail.com>
2020-12-30 17:55:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import time
-import mysql.connector
-import mysqlconnection
+
+import sqlconn
 from roominfo import room_description
 
 # TODO
 # add Jordan animations.
 # setup a map system.
-
-conn = mysqlconnection.get_connection() # This will always return the same object
-
-#SETUP FIRST ROOM AND SQL CONNECTION
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="olivander",
-  password="olivander0",
-  database="quest"
-)
-
-mycursor = mydb.cursor()
 
 class Player:
 	def __init__(self, locationx=0, locationy=0):
@@ -26,17 +14,17 @@ class Player:
 		
 	def load(self):
 		#grabs the first player in db
-		mycursor.execute("SELECT * FROM player WHERE id = 1")
-		newplayer = mycursor.fetchone()
+		
+		query = 'SELECT * FROM player WHERE id = 1'
+		newplayer = sqlconn.execute_query(query)
+		
 		self.locationx = newplayer[1]
 		self.locationy = newplayer[2]
-
-
-
+		print('\n player loaded')
 
 #Create our main player
 player1 = Player()
 player1.load()
-room_description(mycursor, player1.locationx, player1.locationy)
+room_description(player1.locationx, player1.locationy)
 
 
