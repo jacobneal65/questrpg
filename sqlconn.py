2ad256fe5dc805fa73ae0ece5c939fccdc13f08f
Jacob <jacobneal65@gmail.com>
2021-01-01 13:27:31 -0600

    added a multiple return option
    added multiple return parameter for the execute_query method.

--- sqlconn.py
+++ sqlconn.py
@@ -1,24 +1,28 @@
 import mysql.connector
 
-
 #USED TO PULL INFORMATION
-def execute_query(query, tuples=None):
+def execute_query(query, tuples=None, multiple = 0): #multiple parameter is used if you need to pull multiple lines from db
 	conn = mysql.connector.connect(
 				host="localhost",  
 				user="olivander",
 				password="olivander0",
-				database="quest")
-				
+				database="quest")	
 	mycursor = conn.cursor()
 	if tuples != None:
-		print('tuples: '+ str(tuples))
+		#print('tuples: '+ str(tuples))
 		mycursor.execute(query, tuples)
-		returnvals = mycursor.fetchone()
+		if multiple==1:
+			returnvals = mycursor.fetchall()
+		else:
+			returnvals = mycursor.fetchone()
 		mycursor.close()
 		
 	else:
 		mycursor.execute(query)
-		returnvals = mycursor.fetchone()
+		if multiple==1:
+			returnvals = mycursor.fetchall()
+		else:
+			returnvals = mycursor.fetchone()
 	
 	conn.close()
 	return returnvals
@@ -33,10 +37,10 @@ def update_query(query, tuples):
 				
 	mycursor = conn.cursor()
 	mycursor.execute(query, tuples)
-	conn.commit()
+	conn.commit() #commit the changes
 	mycursor.close()
 	conn.close()
-	print('updated')
+	#print('updated')
 	
 
 
