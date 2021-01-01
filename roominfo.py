290165f86ade952000a21acdb5c247611f0de02c
Jacob <jacobneal65@gmail.com>
2021-01-01 13:25:14 -0600

    Major update to roominfo

--- roominfo.py
+++ roominfo.py
@@ -1,16 +1,16 @@
 import sqlconn
+import time
 
-#Gets the information for a room and then waits for user input.
+#MAIN LOOP OF GAME. Gets room descriptions and user inputs etc
 def room_description(x, y):
-	
 	
 	query = 'CALL RoomInfo(%s,%s);'
 	room_tuple = (x, y)
 	currentroom = sqlconn.execute_query(query,room_tuple)
 	
 	print('\n' + currentroom[2] + ' ')
-	
-	direct = '' #initialize
+	#initialize
+	direct = ''
 	if currentroom[3] != 0: #north
 		direct += 'n'
 	if currentroom[4] != 0:
@@ -25,51 +25,76 @@ def room_description(x, y):
 		if len(direct) != 0:
 			direct += ' ' #addspace
 		direct += 'w'
-
 	if len(direct) != 0:
 		print('directions: ' + direct)
-	
-	#USER INPUT
+	user_input(x,y)
+
+
+
+# METHOD TO HANDLE USER INPUT
+def user_input(x,y):
 	choice = input('Input: ')
 	if choice == 'exit' or choice == 'quit':
 		quit()
-	if choice == 'n' or choice == 's' or choice == 'e' or choice == 'w':
+	elif choice == 'restart':
+			sqlconn.update_query( 'CALL UpdatePlayer(%s,%s);',(1,1))	#restart game
+			room_description(1,1)
+	elif choice == 'n' or choice == 's' or choice == 'e' or choice == 'w':
 		if choice == 'n':
-			y+=1
+			newy = y+1
+			newx = x
 		elif choice == 's':
-			y-=1
+			newy = y-1
+			newx = x
 		elif choice == 'e':
-			x+=1
+			newx = x+1
+			newy = y
 		elif choice == 'w':
-			x-=1
-			
+			newx = x-1
 		#run the script for checking room
-		
-		newcoor = (x, y)
+		query = 'CALL RoomInfo(%s,%s);'
+		newcoor = (newx, newy)
 		currentroom = sqlconn.execute_query(query,newcoor)
-	
+		
 		if currentroom is None:
 			print('can\'t go that way')
+			user_input(x, y) 
 		else:
 			#if true run script to change player locationx and locationy
-			print('run update player to '+ str(newcoor))
 			query = 'CALL UpdatePlayer(%s,%s);'
-			playera = sqlconn.update_query(query,newcoor)
-			#recursion of the room		
-			room_description(x, y) 	
-			
-			
-
+			sqlconn.update_query(query,newcoor)	
+			room_description(newx, newy) 		
+	else: 
+		#look for unique commands
+		query = 'CALL GetTrigger(%s,%s);'
+		location = (x, y)
+		trigger = sqlconn.execute_query(query,location)
+		if trigger is not None:
+			if choice == trigger[0]:
+				#call cinematics
+				play_cinematics(trigger[3])
+				#run new room stuff
+				newx=trigger[1]
+				newy=trigger[2]
+				newcoor = (newx, newy)
+				#change player location to new room
+				query = 'CALL UpdatePlayer(%s,%s);'
+				playera = sqlconn.update_query(query,newcoor)
+				room_description(newx, newy) 	
+			else:
+				print('\n nothing happened')
+				user_input(x, y) 
+		else:
+			print('\n nothing happened')
+			user_input(x, y) 
 		
 		
-	else:
-		#run script for checking special commands
-		print('not valid input. Maybe he will add more in the future')
-		room_description(x, y) 
-
-
-
-
-
-
-
+# METHOD TO HANDLE CINEMATICS	
+def play_cinematics(cinid):
+	query = 'CALL GetCinematics(%s);'
+	cintuple = (cinid,)
+	cin = sqlconn.execute_query(query, cintuple,1)	
+	if	cin is not None:
+		for x in cin:
+			print('\n'+x[0])
+			time.sleep(1)
