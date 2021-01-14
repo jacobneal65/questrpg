7f5688d87de9a61a5200e9ecf0cc5bd4081255b5
Jacob <jacobneal65@gmail.com>
2021-01-14 00:11:31 -0600

    Update to SQLite

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 import time
 
-import sqlconn
+import sqlcmds
 from roominfo import room_description
 
 # TODO
 # add Jordan animations.
-# setup a map system.
 
-class Player:
-	def __init__(self, locationx=0, locationy=0):
-		self.locationx = locationx
-		self. locationy = locationy
-		
-	def load(self):
-		#grabs the first player in db
-		
-		query = 'SELECT * FROM player WHERE id = 1'
-		newplayer = sqlconn.execute_query(query)
-		
-		self.locationx = newplayer[1]
-		self.locationy = newplayer[2]
-		print('\n player loaded')
-		time.sleep(0.8)
-#Create our main player
-player1 = Player()
-player1.load()
-room_description(player1.locationx, player1.locationy)
+
+#--------BEGINNING--------
+# Load the player and begin game loop
+p = sqlcmds.load_player()
+room_description(p[1], p[2])
 
 
