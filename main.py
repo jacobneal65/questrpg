75a045e415588ce86e7a2ef3e3b9c5c2e62ada59
Jacob <jacobneal65@gmail.com>
2021-10-18 23:58:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import time
-import sqlcmds
-from roominfo import room_description
+from sql_pkg import sqlcmds
+from room_pkg import roominfo
 #--------BEGINNING--------
 # Load the player and begin game loop
 def main():
-	p = sqlcmds.load_player()
-	room_description(p[1], p[2])
+	p = sqlcmds.load_player() #load player p
+	roominfo.room_description(p[1], p[2])
 
 
 main()
